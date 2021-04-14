01b4e4a7036a6b743b6b55f6391252235b608468
Luis Morquecho <luisiyi1997@gmail.com>
2021-04-14 04:03:05 -0700

    Primer 3 pantallas realizadas.

---
+++ app/src/main/java/molina/raul/projectotoc/Log_in.kt
@@ -0,0 +1,11 @@
+package molina.raul.projectotoc
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Log_in : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_log_in)
+    }
+}
